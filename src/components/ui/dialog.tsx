f7c0fe640607a782ad6a8d6c724af0c4b7f57ccb
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-10-24 05:38:48 +0000

    the transitions between modals are still very abrupt and jaggy. ENsure t

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-300 data-[state=open]:animate-modal-in data-[state=closed]:animate-modal-out sm:rounded-lg",
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg data-[state=open]:animate-modal-in data-[state=closed]:animate-modal-out sm:rounded-lg",
         className
       )}
       {...props}
