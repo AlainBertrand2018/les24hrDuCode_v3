7ec99651dff15bb3891d7c01f7defd0920c7ce96
Alain BERTRAND <alain.bertrand.mu@gmail.com>
2025-10-26 15:05:20 +0000

    replace Ministry logo with public/images/logos/mitci_marqueeLogo.png
    Re

--- src/components/logo-marquee.tsx
+++ src/components/logo-marquee.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import Marquee from 'react-fast-marquee';
 
 const logos = [
-    { id: 'd1', name: 'Ministry of IT', logo_url: '/images/Coat_of_arms_of_Mauritius_(Original_version).svg.png' },
+    { id: 'd1', name: 'Ministry of IT', logo_url: '/images/logos/mitci_marqueeLogo.png' },
     { id: 'd2', name: 'EDB', logo_url: '/images/logos/edb_logo.png' },
     { id: 'd3', name: 'Dell', logo_url: '/images/logos/Dell_Logo.png' },
     { id: 'd4', name: 'Le Labourdonnais Hotel', logo_url: '/images/logos/le_labourdonnais_logo.png' },
     { id: 'p1', name: 'Google Gemini', logo_url: '/images/logos/Google_Gemini_logo.png' },
-    { id: 'p2', name: 'Microsoft Azure', logo_url: '/images/logos/microsoft_azure.png' },
+    { id: 'p2', name: 'Microsoft Azure', logo_url: '/images/logos/msAzure_marqueeLogo.png' },
     { id: 'p3', name: 'OpenAI', logo_url: '/images/logos/openai_wht_transp.png' },
     { id: 'p4', name: 'Mistral AI', logo_url: '/images/logos/mistral_wht_transp.png' },
     { id: 'p5', name: 'Anthropic', logo_url: '/images/logos/anthr_wht_transp.png' },
